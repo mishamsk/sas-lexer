bdee3159c3981477f90b4cc10d019983e45ff814
mishamsk <mishamsk@gmail.com>
2024-07-27 19:24:16 -0400

    continuity test on samples

--- tests/test_sample_files.rs
+++ tests/test_sample_files.rs
@@ -7,7 +7,7 @@ use sas_lexer::{lex, print::print_token};
 use std::{fs, path::PathBuf};
 
 #[rstest]
-fn test_files(#[files("tests/samples/**/*.sas")] path: PathBuf) {
+fn test_snapshots(#[files("tests/samples/**/*.sas")] path: PathBuf) {
     // Compute the absolute path of the prefix
     let prefix = fs::canonicalize("tests/samples/").unwrap();
 
@@ -27,4 +27,37 @@ fn test_files(#[files("tests/samples/**/*.sas")] path: PathBuf) {
         .collect();
 
     assert_yaml_snapshot!(tokens);
+}
+
+/// Tests that tokens cover all offsets in the file from 0 to the end
+#[rstest]
+fn test_full_coverage(#[files("tests/samples/**/*.sas")] path: PathBuf) {
+    let contents = fs::read_to_string(&path).unwrap();
+
+    let tok_buffer = lex(contents.as_str());
+
+    let mut end = 0;
+
+    for token in tok_buffer.into_iter() {
+        // Check that the token starts where the previous token ended
+        assert_eq!(
+            tok_buffer.get_token_start(token),
+            end,
+            "Token <{}> does not start where the previous token ended",
+            print_token(token, &tok_buffer)
+        );
+
+        // Set the new end
+        end = tok_buffer.get_token_end(token);
+
+        // Check that the end is greater than the start
+        assert!(
+            end >= tok_buffer.get_token_start(token),
+            "Token <{}> has an end before the start",
+            print_token(token, &tok_buffer)
+        );
+    }
+
+    // Check that the last token ends at the end of the file
+    assert_eq!(end, contents.len() as u32);
 }
