44629b7b12f1e867707b3d9bab4d59d88e9ad470
mishamsk <mishamsk@gmail.com>
2024-07-28 21:06:56 -0400

    added char offsets

--- crates/sas_lexer/src/lexer/print.rs
+++ crates/sas_lexer/src/lexer/print.rs
@@ -3,12 +3,13 @@ use crate::lexer::buffer::{Payload, TokenIdx, TokenizedBuffer};
 
 #[must_use]
 pub fn to_pretty_string(token: TokenIdx, buffer: &TokenizedBuffer) -> String {
+    let token_idx = token.get();
     let start_line = buffer.get_token_start_line(token);
     let end_line = buffer.get_token_end_line(token);
     let start_column = buffer.get_token_start_column(token);
     let end_column = buffer.get_token_end_column(token);
-    let token_start = buffer.get_token_start(token);
-    let token_end = buffer.get_token_end(token);
+    let token_start = buffer.get_token_start(token).get();
+    let token_end = buffer.get_token_end(token).get();
     let token_text = buffer.get_token_text(token).unwrap_or("<no text>");
     let token_type = buffer.get_token_type(token);
     let token_channel = buffer.get_token_channel(token);
@@ -21,7 +22,7 @@ pub fn to_pretty_string(token: TokenIdx, buffer: &TokenizedBuffer) -> String {
 
     // Constructing the string representation of the token
     let token_repr = format!(
-        "[@{token},{token_start}:{token_end}={token_text:?},<{token_type}>,\
+        "[@{token_idx},{token_start}:{token_end}={token_text:?},<{token_type}>,\
         L{start_line}:C{start_column}-L{end_line}:C{end_column},chl={token_channel},\
         pl={payload_str:?}]"
     );
--- crates/sas_lexer/tests/util.rs
+++ crates/sas_lexer/tests/util.rs
@@ -1,3 +1,7 @@
+#![allow(dead_code)]
+
+use sas_lexer::{Payload, TokenChannel, TokenIdx, TokenType, TokenizedBuffer};
+
 #[macro_export]
 macro_rules! set_snapshot_suffix {
     ($($expr:expr),*) => {
@@ -5,4 +9,122 @@ macro_rules! set_snapshot_suffix {
         settings.set_snapshot_suffix(format!($($expr,)*));
         let _guard = settings.bind_to_scope();
     }
+}
+
+pub(crate) fn check_token(
+    token: TokenIdx,
+    buffer: &TokenizedBuffer,
+    start_byte_offset: u32,
+    end_byte_offset: u32,
+    start_char_offset: u32,
+    end_char_offset: u32,
+    start_line: u32,
+    line_count: u32,
+    start_column: u32,
+    end_column: u32,
+    token_type: TokenType,
+    token_channel: TokenChannel,
+    payload: Payload,
+    token_text: Option<&str>,
+) {
+    // Byte offsets
+    assert_eq!(
+        buffer.get_token_start_byte_offset(token).get(),
+        start_byte_offset,
+        "Expected start byte offset {}, got {}",
+        start_byte_offset,
+        buffer.get_token_start(token).get()
+    );
+
+    assert_eq!(
+        buffer.get_token_end_byte_offset(token).get(),
+        end_byte_offset,
+        "Expected end byte offset {}, got {}",
+        end_byte_offset,
+        buffer.get_token_end(token).get()
+    );
+
+    // Char offsets
+    assert_eq!(
+        buffer.get_token_start(token).get(),
+        start_char_offset,
+        "Expected start char offset {}, got {}",
+        start_char_offset,
+        buffer.get_token_start(token).get()
+    );
+    assert_eq!(
+        buffer.get_token_end(token).get(),
+        end_char_offset,
+        "Expected end char offset {}, got {}",
+        end_char_offset,
+        buffer.get_token_end(token).get()
+    );
+
+    // Line and column
+    assert_eq!(
+        buffer.get_token_start_line(token),
+        start_line,
+        "Expected start line {}, got {}",
+        start_line,
+        buffer.get_token_start_line(token)
+    );
+
+    let end_line = start_line + line_count - 1;
+
+    assert_eq!(
+        buffer.get_token_end_line(token),
+        end_line,
+        "Expected end line {}, got {}",
+        end_line,
+        buffer.get_token_end_line(token)
+    );
+
+    assert_eq!(
+        buffer.get_token_start_column(token),
+        start_column,
+        "Expected start column {}, got {}",
+        start_column,
+        buffer.get_token_start_column(token)
+    );
+
+    assert_eq!(
+        buffer.get_token_end_column(token),
+        end_column,
+        "Expected end column {}, got {}",
+        end_column,
+        buffer.get_token_end_column(token)
+    );
+
+    // Token type, channel, payload and text
+    assert_eq!(
+        buffer.get_token_type(token),
+        token_type,
+        "Expected token type {:?}, got {:?}",
+        token_type,
+        buffer.get_token_type(token)
+    );
+
+    assert_eq!(
+        buffer.get_token_channel(token),
+        token_channel,
+        "Expected token channel {:?}, got {:?}",
+        token_channel,
+        buffer.get_token_channel(token)
+    );
+
+    assert_eq!(
+        buffer.get_token_payload(token),
+        payload,
+        "Expected token payload {:?}, got {:?}",
+        payload,
+        buffer.get_token_payload(token)
+    );
+
+    assert_eq!(
+        buffer.get_token_text(token),
+        token_text,
+        "Expected text {:?}, got {:?}",
+        token_text,
+        buffer.get_token_text(token)
+    );
 }
