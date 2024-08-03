8f6a6721e378dbab24877863ded94b9dbc674a20
mishamsk <mishamsk@gmail.com>
2024-08-03 13:10:26 -0400

    new testing framework

--- crates/sas_lexer/tests/util.rs
+++ crates/sas_lexer/tests/util.rs
@@ -2,7 +2,7 @@
 
 use sas_lexer::{
     error::{ErrorInfo, ErrorType},
-    Payload, TokenChannel, TokenIdx, TokenType, TokenizedBuffer,
+    lex, Payload, TokenChannel, TokenIdx, TokenType, TokenizedBuffer,
 };
 
 #[macro_export]
@@ -14,7 +14,132 @@ macro_rules! set_snapshot_suffix {
     }
 }
 
-pub(crate) fn check_token(
+pub(crate) trait TokenTestCase {
+    fn token_type(&self) -> TokenType;
+    fn token_channel(&self) -> TokenChannel;
+    fn payload(&self) -> Payload;
+    fn text<S: AsRef<str>>(&self, source: S) -> Option<String>;
+}
+
+impl TokenTestCase for (&str, TokenType, TokenChannel) {
+    fn token_type(&self) -> TokenType {
+        self.1
+    }
+
+    fn token_channel(&self) -> TokenChannel {
+        self.2
+    }
+
+    fn payload(&self) -> Payload {
+        Payload::None
+    }
+
+    fn text<S: AsRef<str>>(&self, _source: S) -> Option<String> {
+        if self.0.is_empty() {
+            None
+        } else {
+            Some(self.0.to_owned())
+        }
+    }
+}
+
+impl TokenTestCase for (&str, TokenType) {
+    fn token_type(&self) -> TokenType {
+        self.1
+    }
+
+    fn token_channel(&self) -> TokenChannel {
+        assert_ne!(
+            self.1,
+            TokenType::WS,
+            "Whitespace token must have a channel"
+        );
+        TokenChannel::default()
+    }
+
+    fn payload(&self) -> Payload {
+        Payload::None
+    }
+
+    fn text<S: AsRef<str>>(&self, _source: S) -> Option<String> {
+        if self.0.is_empty() {
+            None
+        } else {
+            Some(self.0.to_owned())
+        }
+    }
+}
+
+impl TokenTestCase for (TokenType, TokenChannel) {
+    fn token_type(&self) -> TokenType {
+        self.0
+    }
+
+    fn token_channel(&self) -> TokenChannel {
+        self.1
+    }
+
+    fn payload(&self) -> Payload {
+        Payload::None
+    }
+
+    fn text<S: AsRef<str>>(&self, source: S) -> Option<String> {
+        // If a test case doesn't provide a text, it means the whole source
+        // is the token text
+        if source.as_ref().is_empty() {
+            None
+        } else {
+            Some(source.as_ref().to_owned())
+        }
+    }
+}
+
+impl TokenTestCase for TokenType {
+    fn token_type(&self) -> TokenType {
+        *self
+    }
+
+    fn token_channel(&self) -> TokenChannel {
+        match self {
+            TokenType::WS => TokenChannel::HIDDEN,
+            _ => TokenChannel::default(),
+        }
+    }
+
+    fn payload(&self) -> Payload {
+        Payload::None
+    }
+
+    fn text<S: AsRef<str>>(&self, source: S) -> Option<String> {
+        // If a test case doesn't provide a text, it means the whole source
+        // is the token text
+        match self {
+            TokenType::EOF => None,
+            TokenType::WS => {
+                // Special case for automatic whitespace token text
+                // Just find the first non-whitespace character
+                let text = source.as_ref();
+                let end = text
+                    .find(|c: char| !c.is_whitespace())
+                    .unwrap_or(source.as_ref().len());
+
+                // This is a bug in the test case
+                assert_ne!(end, 0, "Whitespace token text is empty");
+
+                Some(text[..end].to_owned())
+            }
+            _ => {
+                if source.as_ref().is_empty() {
+                    None
+                } else {
+                    Some(source.as_ref().to_owned())
+                }
+            }
+        }
+    }
+}
+
+pub(crate) fn check_token<S: AsRef<str>>(
     source: &str,
     token: TokenIdx,
     buffer: &TokenizedBuffer,
@@ -29,7 +154,7 @@ pub(crate) fn check_token(
     token_type: TokenType,
     token_channel: TokenChannel,
     payload: Payload,
-    token_text: Option<&str>,
+    token_text: Option<S>,
 ) {
     assert_ne!(line_count, 0, "Line count must be greater than 0");
 
@@ -126,6 +251,8 @@ pub(crate) fn check_token(
         buffer.get_token_payload(token)
     );
 
+    let token_text = token_text.as_ref().map(|s| s.as_ref());
+
     assert_eq!(
         buffer.get_token_text(token, &source),
         token_text,
@@ -133,6 +260,36 @@ pub(crate) fn check_token(
         token_text,
         buffer.get_token_text(token, &source)
     );
+}
+
+pub(crate) trait ErrorTestCase {
+    fn error_type(&self) -> ErrorType;
+
+    fn at_byte_offset(&self, last_byte_offset: u32) -> u32 {
+        last_byte_offset
+    }
+
+    fn at_char_offset(&self, last_char_offset: u32) -> u32 {
+        last_char_offset
+    }
+
+    fn on_line(&self, last_line: u32) -> u32 {
+        last_line
+    }
+
+    fn at_column(&self, last_column: u32) -> u32 {
+        last_column
+    }
+
+    fn last_token_idx(&self, ast_non_eof_token: Option<TokenIdx>) -> Option<TokenIdx> {
+        ast_non_eof_token
+    }
+}
+
+impl ErrorTestCase for ErrorType {
+    fn error_type(&self) -> ErrorType {
+        *self
+    }
 }
 
 pub(crate) fn check_error(
@@ -191,4 +348,149 @@ pub(crate) fn check_error(
         "Expected last token index {:?}, got {:?}",
         last_token_idx, last_token
     );
+}
+
+pub(crate) fn assert_lexing(
+    source: &str,
+    expected_tokens: Vec<impl TokenTestCase>,
+    expected_errors: Vec<impl ErrorTestCase>,
+) {
+    let (buffer, errors) = lex(&source).unwrap();
+
+    // Check tokens
+    let tokens: Vec<TokenIdx> = buffer.into_iter().collect();
+
+    // Save last_non_eof_token_idx for error checking below
+    let last_non_eof_token_idx = tokens
+        .iter()
+        .rev()
+        .filter(|&t| buffer.get_token_type(*t) != TokenType::EOF)
+        .last()
+        .copied();
+
+    // Check total token count
+    assert_eq!(
+        tokens.len(),
+        expected_tokens.len() + 1,
+        "Expected {} tokens including EOF, got {}",
+        expected_tokens.len() + 1,
+        tokens.len()
+    );
+
+    // reverse the tokens, so we could compare left to right
+    let mut tokens = tokens.into_iter().rev().collect::<Vec<_>>();
+
+    let mut cur_start_line = 1u32;
+    let mut cur_start_byte_offset = 0u32;
+    let mut cur_start_char_offset = 0u32;
+    let mut cur_start_column = 0u32;
+
+    for expected_tok in expected_tokens {
+        let lexed_token = tokens.pop().unwrap();
+
+        // Calculate the remaining test text
+        let rem_source = &source[cur_start_byte_offset as usize..];
+
+        // Get the expected token text
+        let expected_tok_text = expected_tok.text(rem_source);
+
+        // Calculate various offsets
+        let cur_end_byte_offset = cur_start_byte_offset
+            + expected_tok_text
+                .as_ref()
+                .map_or(0, |text| text.len() as u32);
+
+        let text_tok_chars = expected_tok_text
+            .as_ref()
+            .map_or(0, |text| text.chars().count() as u32);
+
+        let cur_end_char_offset = cur_start_char_offset + text_tok_chars;
+
+        let line_count = expected_tok_text
+            .as_ref()
+            .map_or(1, |text| text.lines().count() as u32);
+
+        let ends_with_newline = expected_tok_text
+            .as_ref()
+            .map_or(false, |text| text.ends_with('\n'));
+
+        let cur_end_line = cur_start_line + line_count - 1;
+
+        let cur_end_column = match expected_tok_text.as_ref() {
+            Some(text) => {
+                if line_count > 1 {
+                    text.lines().last().unwrap().chars().count() as u32
+                        + u32::from(ends_with_newline)
+                } else {
+                    cur_start_column + text_tok_chars
+                }
+            }
+            None => cur_start_column,
+        };
+
+        check_token(
+            source,
+            lexed_token,
+            &buffer,
+            cur_start_byte_offset,
+            cur_end_byte_offset,
+            cur_start_char_offset,
+            cur_end_char_offset,
+            cur_start_line,
+            line_count,
+            cur_start_column,
+            cur_end_column,
+            expected_tok.token_type(),
+            expected_tok.token_channel(),
+            expected_tok.payload(),
+            expected_tok_text.as_ref(),
+        );
+
+        cur_start_line = cur_end_line + u32::from(ends_with_newline);
+        cur_start_byte_offset = cur_end_byte_offset;
+        cur_start_char_offset = cur_end_char_offset;
+        cur_start_column = if ends_with_newline { 0 } else { cur_end_column };
+    }
+
+    // Chec EOF token
+    check_token::<String>(
+        source,
+        tokens.pop().unwrap(),
+        &buffer,
+        cur_start_byte_offset,
+        cur_start_byte_offset,
+        cur_start_char_offset,
+        cur_start_char_offset,
+        cur_start_line,
+        1,
+        cur_start_column,
+        cur_start_column,
+        TokenType::EOF,
+        TokenChannel::DEFAULT,
+        Payload::None,
+        None,
+    );
+
+    // Check errors
+    assert_eq!(
+        expected_errors.len(),
+        errors.len(),
+        "Expected {} errors, got {}",
+        expected_errors.len(),
+        errors.len()
+    );
+
+    for (i, expected_err) in expected_errors.iter().enumerate() {
+        let lexed_err = errors.get(i).unwrap();
+
+        check_error(
+            lexed_err,
+            expected_err.error_type(),
+            expected_err.at_byte_offset(cur_start_byte_offset),
+            expected_err.at_char_offset(cur_start_char_offset),
+            expected_err.on_line(cur_start_line),
+            expected_err.at_column(cur_start_column),
+            expected_err.last_token_idx(last_non_eof_token_idx),
+        );
+    }
 }
