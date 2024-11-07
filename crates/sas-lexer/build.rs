a427f945b312bd15faa382be96475e205133761a
mishamsk <mishamsk@gmail.com>
2024-11-06 20:25:34 -0500

    ⬇️ reduce msrv 💚 add msrv test to CI 🛠️ 🔥 remove slow udeps check from pre-commit 🛠️ do workspace check in pre-commit

--- crates/sas-lexer/build.rs
+++ crates/sas-lexer/build.rs
@@ -3,7 +3,7 @@ extern crate rustc_version;
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    println!("cargo::rustc-check-cfg=cfg(rustc_nightly)");
+    println!("cargo:rustc-check-cfg=cfg(rustc_nightly)");
 
     let version_meta = version_meta().expect("No rustc version found!");
     if version_meta.channel == Channel::Nightly {
